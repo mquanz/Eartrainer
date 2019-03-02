05a82119d141228b3b1b76d93b99fbef9b3ad5d2
dnotb <36969591+dnotb@users.noreply.github.com>
2019-03-02 13:41:50 +0100

    added simpleaudio vaw opening

--- src/eartrainer.py
+++ src/eartrainer.py
@@ -1,5 +1,7 @@
 print('Welcome to EARTRAINER - developed by Richard and Martin!')
 
-import wave
+import simpleaudio as sa
 
-
+wave_obj = sa.WaveObject.from_wave_file("../media/test.wav")
+play_obj = wave_obj.play()
+play_obj.wait_done()
