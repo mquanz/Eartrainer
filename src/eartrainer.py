92f68d238c37641c3f4cec012e9b8af01ce28b74
dnotb <36969591+dnotb@users.noreply.github.com>
2019-03-02 14:40:38 +0100

    random choice of soundfile

--- src/eartrainer.py
+++ src/eartrainer.py
@@ -1,7 +1,14 @@
 print('Welcome to EARTRAINER - developed by Richard and Martin!')
 
 import simpleaudio as sa
+import os
+import random
 
-wave_obj = sa.WaveObject.from_wave_file("../media/test.wav")
+sounds = os.listdir('../media')
+
+media = random.choice(sounds)
+wave_obj = sa.WaveObject.from_wave_file("../media/" + media)
 play_obj = wave_obj.play()
 play_obj.wait_done()
+
+print(media)
