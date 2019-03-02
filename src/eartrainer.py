fe3038fabf3daa90db24b34e771c56ac647be474
dnotb <36969591+dnotb@users.noreply.github.com>
2019-03-02 14:59:32 +0100

    added user interaction

--- src/eartrainer.py
+++ src/eartrainer.py
@@ -1,14 +1,21 @@
-print('Welcome to EARTRAINER - developed by Richard and Martin!')
-
 import simpleaudio as sa
 import os
 import random
 
-sounds = os.listdir('../media')
+print("Hello, do you want to train? Enter 'y' for Yes or 'n' for No")
+user_input = input(">")
+if user_input == "y":
+    sounds = os.listdir("../media")
 
-media = random.choice(sounds)
-wave_obj = sa.WaveObject.from_wave_file("../media/" + media)
-play_obj = wave_obj.play()
-play_obj.wait_done()
+    media = random.choice(sounds)
+    wave_obj = sa.WaveObject.from_wave_file("../media/" + media)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
 
-print(media)
+    user_guess = input("What did you hear?")
+    if user_guess == media[0:len(media)-4]:
+        print("right, it's " + media)
+    else:
+        print("wrong")
+
+
