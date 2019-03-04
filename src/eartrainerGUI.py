36dad8f527c6f800f4b2ac7e03976312d562e8c8
dnotb <36969591+dnotb@users.noreply.github.com>
2019-03-04 19:21:21 +0100

    enabling user to choose between repeat and play

--- src/eartrainerGUI.py
+++ src/eartrainerGUI.py
@@ -16,14 +16,23 @@ class Window:
         self.label1 = tk.Label(master, text = 'Welcome to the famous Eartrainer - created by M & R', font = ('Times', 14), bd = 15)
         self.label1.grid(row = 0, columnspan = 2)
 
-        self.play_button = tk.Button(master, text = 'PLAY', bg = 'green', command = self.Play_Interval)
-        self.play_button.grid(row = 2, column = 1)
+        self.repeat_button = tk.Button(master, text = 'Repeat Interval', command = self.Repeat_Interval)
+        self.repeat_button.grid(row = 2, column = 1)
+        
+        self.play_button = tk.Button(master, text = 'Play new Interval', command = self.Play_Interval)
+        self.play_button.grid(row = 2, column = 2)
 
         self.entry = tk.Entry(master, width = 32, fg = 'grey')
         self.entry.insert(0, 'Enter your guess here...')
         self.entry.bind('<Button-1>', lambda event: self.entry_click(event, 'Enter your guess here...'))
         self.entry.bind('<Return>', lambda event: self.Control())
         self.entry.grid(row = 1)
+    
+    def Repeat_Interval(self):
+        wave_obj = sa.WaveObject.from_wave_file("../media/" + media)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+        print(media)
     
     def Play_Interval(self):
         sounds = os.listdir("../media")
