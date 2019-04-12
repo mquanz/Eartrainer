f47aa6e5edd1789e5f9998976e44f5d17c0095df
Martin <martin.quanz@gmail.com>
2019-04-12 17:32:36 +0200

    deleted entry field and introduced checkbuttons

--- src/eartrainerGUI.py
+++ src/eartrainerGUI.py
@@ -28,12 +28,6 @@ class Window:
         self.check_button = tk.Button(master, text = 'CHECK', bg = 'orange', command = self.Control)
         self.check_button.grid(row = 1, column = 1)
 
-        self.entry = tk.Entry(master, width = 32, fg = 'grey')
-        self.entry.insert(0, 'Enter your guess here...')
-        self.entry.bind('<Button-1>', lambda event: self.entry_click(event, 'Enter your guess here...'))
-        self.entry.bind('<Return>', lambda event: self.Control())
-        self.entry.grid(row = 1)
-    
     def Previous_Interval(self):
         global counter
         if counter > 0:
@@ -53,16 +47,29 @@ class Window:
         play_obj = wave_obj.play()
         play_obj.wait_done()
         print(media, media[counter])
+        
+        # Possible rows for placement of checkbuttons are 3, 4 and 5.
+        rows = [3,4,5]
+        # True answer will be placed randomly.
+        truerow = random.choice(rows)
+        # Remove true row from list for placement of wrong checkbuttons.
+        rows.remove(truerow)
+        # Create list without true answer for labels of wrong heckbuttons and shuffle it.
+        labels = random.sample([x for i,x in enumerate(media) if i!=counter], len(media)-1)
+        # Checkbox1 always contains true answer.
+        self.checkbox1 = tk.IntVar()
+        tk.Checkbutton(text = media[counter], variable = self.checkbox1).grid(row = truerow)
+        self.checkbox2 = tk.IntVar()
+        tk.Checkbutton(text = labels[0], variable = self.checkbox2).grid(row = rows[0])
+        self.checkbox3 = tk.IntVar()
+        tk.Checkbutton(text = labels[1], variable = self.checkbox3).grid(row = rows[1])
 
     def Control(self):
-        user_input = self.entry.get() + ".wav"
-        try:
-            if user_input == media[counter]:
-                messagebox.showinfo('Result', 'Well done, it was ' + media[counter] + '! :)')
-            else:
-                messagebox.showinfo('Result', 'Youre wrong, it was ' + media[counter] + '! :(')
-        except NameError:
-                    messagebox.showinfo('Error', 'Enter your guess first')
+        user_input = self.checkbox1.get()
+        if user_input == 1:
+            messagebox.showinfo('Result', 'Well done, it was ' + media[counter] + '! :)')
+        else:
+            messagebox.showinfo('Result', 'Youre wrong, it was ' + media[counter] + '! :(')
         
     def entry_click(self, event, default_text):
             self.entry.delete(0, tk.END)
