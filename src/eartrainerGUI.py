f0c6a16cda3b8b96d6fc9643ceefdd560669e1a3
Martin <martin.quanz@gmail.com>
2019-06-03 17:25:16 +0200

    implemented combobox and deleted checkboxes

--- src/eartrainerGUI.py
+++ src/eartrainerGUI.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter.ttk import Combobox
 import simpleaudio as sa
 import os
 import random
 
 media = random.sample(os.listdir("../media"), len(os.listdir("../media")))
+
 dic = {'C0.wav':'Prime', 'C1.wav':'kl. Sekunde', 'C1r.wav':'kl. Sekunde', 'C2.wav':'gr. Sekunde','C2r.wav':'gr. Sekunde', 'C3.wav':'kl. Terz', 'C3r.wav':'kl. Terz', 'C4.wav':'gr. Terz', 'C4r.wav':'gr. Terz', 'C5.wav':'r. Quarte', 'C5r.wav':'r. Quarte', 'C6.wav':'verm. Quinte', 'C6r.wav':'verm. Quinte', 'C7.wav':'r. Quinte', 'C7r.wav':'r. Quinte', 'C8.wav':'kl. Sexte', 'C8r.wav':'kl. Sexte', 'C9.wav':'gr. Sexte', 'C9r.wav':'gr. Sexte', 'C10.wav':'kl. Septime', 'C10r.wav':'kl. Septime', 'C11.wav':'gr. Septime', 'C11r.wav':'gr. Septime', 'C12.wav':'Oktave', 'C12r.wav':'Oktave'}
 counter = 0
 
@@ -14,7 +16,7 @@ class Window:
         self.master = master
         master.title('Eartrainer')
         
-        self.label1 = tk.Label(master, text = 'Welcome to the famous Eartrainer - created by M & R', font = ('Times', 14), bd = 18, bg = 'white')
+        self.label1 = tk.Label(master, text = 'Welcome to the famous Eartrainer - created by M & R', font = ('Arial Bold', 14), bd = 18, bg = 'white')
         self.label1.grid(row = 0, columnspan = 2)
         
         self.previous_button = tk.Button(master, text = 'PREVIOUS', bg = 'grey', bd = 5, command = self.Previous_Interval)
@@ -29,14 +31,14 @@ class Window:
         self.check_button = tk.Button(master, text = 'CHECK', bg = 'orange', bd = 5, command = self.Check_Answer)
         self.check_button.grid(row = 2, column = 1)
         
-        self.create_checkboxes()
+        self.combo = Combobox(master)
+        self.combo['values'] = sorted(list(set([dic[x] for x in media])))
+        self.combo.grid(row = 3)
 
     def Previous_Interval(self):
         global counter
         if counter > 0:
             counter -= 1
-            print(media, media[counter])
-            self.create_checkboxes()
         else:
             messagebox.showinfo('Error', 'Counter too low')
             counter = 0
@@ -45,12 +47,9 @@ class Window:
         global counter
         if counter < len(media)-1:
             counter += 1
-            print(media, media[counter])
-            self.create_checkboxes()
         else:
             messagebox.showinfo('End of Trainng', '''Congrats! You've trained hard. List is replaying now.''' )
             counter = 0
-            print(media, media[counter])
 
     def Play_Interval(self):
         wave_obj = sa.WaveObject.from_wave_file("../media/" + media[counter])
@@ -59,31 +58,11 @@ class Window:
         print(media, media[counter])
         
     def Check_Answer(self):
-        user_input = [self.checkbox1.get(), self.checkbox2.get(), self.checkbox3.get()]
-        # When (only) messagebox1 is true, show right answer.
-        if user_input[0] == 1 and user_input[1] == 0 and user_input[2] == 0:
+        user_input = self.combo.get()
+        if user_input == dic[media[counter]]:
             messagebox.showinfo('Result', 'Well done, it was a ' + dic[media[counter]] + '! :)')
         else:
-            messagebox.showinfo('Result', 'You are wrong or tried to enter multiple answers. Please try again.')
-            
-    def create_checkboxes(self):
-        # Possible rows for placement of checkbuttons are 3, 4 and 5.
-        rows = [3,4,5]
-        # True answer will be placed randomly.
-        truerow = random.choice(rows)
-        # Remove true row from list for placement of wrong checkbuttons.
-        rows.remove(truerow)        
-        trueinterval = media[counter]
-        # Create list without true answer for labels of wrong ceckbuttons and shuffle it.
-        labels = random.sample([x for x in media if dic[x] != dic[trueinterval]], len(media)-2)
-        # Create checkboxes, checkbox1 always contains true answer.
-        self.checkbox1 = tk.IntVar()
-        tk.Checkbutton(text = dic[media[counter]], variable = self.checkbox1, bg = 'white', width = 20).grid(row = truerow, sticky = 'w')
-        self.checkbox2 = tk.IntVar()
-        tk.Checkbutton(text = dic[labels[0]], variable = self.checkbox2, bg = 'white', width = 20).grid(row = rows[0], sticky = 'w')
-        self.checkbox3 = tk.IntVar()
-        tk.Checkbutton(text = dic[labels[1]], variable = self.checkbox3, bg = 'white', width = 20).grid(row = rows[1], sticky = 'w')
-
+            messagebox.showinfo('Result', 'You are wrong, please try again.')
 
 root = tk.Tk()
 root.configure(background = 'white')
